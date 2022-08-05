e8abe16084b8094264fcdd953c87c0ca3f214817
Abdullahi Elmi <abdullahielmi147@gmail.com>
2022-08-05 15:52:39 -0700

    fix the issue with CI

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ let headNumber = 0;
 let middleNumber = 0;
 let bottomNumber = 0;
 
+let catchphrases = [];
 
 headDropdown.addEventListener('change', () => {
     // get the value of the head dropdown
@@ -25,6 +26,7 @@ headDropdown.addEventListener('change', () => {
     headNumber = headNumber + 1;
     // update the dom for the head (use style.backgroundImage on the headEl div instead of trying to set the .src -- it's NOT an img tag!)
     headEl.style.backgroundImage = `url(/assets/${headDropdown.value}-head.png)`;
+    displayStats();
     
     // update the stats to show the new count (call displayStats() to do this work)
 });
@@ -39,6 +41,7 @@ middleDropdown.addEventListener('change', () => {
     // update the dom for the middle (NOTE: use style.backgroundImage on the middleEl div instead of trying to set the .src -- it's NOT an img tag!)
     middleEl.style.backgroundImage = `url(/assets/${middleDropdown.value}-middle.png)`;
     // update the stats to show the new count (call displayStats() to do this work)
+    displayStats();
 });
 
 
@@ -47,32 +50,48 @@ bottomDropdown.addEventListener('change', () => {
     console.log('bottom was changed');
     console.log(bottomDropdown.value);
     // increment the bottom change count state
-    bottomDropdown = bottomNumber + 1;
+    bottomNumber = bottomNumber + 1;
     // update the dom for the bottom (NOTE use style.backgroundImage on the bottomEl div instead of trying to set the .src -- it's NOT an img tag!)
-    bottomEl.style.backgroundImage = `url(/assets/${bottomDropdown.value}-bottom.png)`;
+    bottomEl.style.backgroundImage = `url(/assets/${bottomDropdown.value}-pants.png)`;
     // update the stats to show the new count (call displayStats() to do this work)
+    displayStats();
 });
 
 catchphraseButton.addEventListener('click', () => {
     // get the value of the catchphrase input
-    
+    const newCatchPhrase = catchphraseInput.value;
     // push the new catchphrase to the catchphrase array in state
-
+    catchphrases.push(newCatchPhrase);
     // clear out the form input's value so it's empty to the user
-   
+    catchphraseInput.value = '';
     // update the dom to show the new catchphrases (refactor to/call displayCatchphrases to do this work)
+    console.log(newCatchPhrase);
+    displayStats();
+    displayCatchphrases();
 
 });
 
 function displayStats() {
-    // text content of the reportEl to tell the user how many times they've changed each piece of the state
+
+    reportEl.textContent = `You have changed the head ${headNumber} times, the body ${middleNumber} times, and the pants ${bottomNumber} times. And your character's catchphrase is:`;
 }
 
-function displayCatchphrases() {
-    // clear out the DOM for the currently displayed catchphrases
+    // text content of the reportEl to tell the user how many times they've changed each piece of the state
 
+function displayCatchphrases() {
+    catchphrasesEl.textContent = '';
+    // clear out the DOM for the currently displayed catchphrases
+    for (let catchphrase of catchphrases) {
+        
+        const newCatchPhrase = document.createElement('p');
+        newCatchPhrase.textContent = catchphrase;
+
+        catchphrasesEl.append(newCatchPhrase);
+        
+    }
+    
     // loop through each catchphrase in state
-   
+
     // and for each catchphrase
     
     // create an HTML element with the catchphrase as its text content
